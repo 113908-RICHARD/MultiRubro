e3d993be098dd9a78ad37b7b3acb670c7afb5d8b
113908-RICHARD <128639135+113908-RICHARD@users.noreply.github.com>
2024-04-02 21:09:29 -0300

    First commit of the backend
    The initial proyect. Nothing wrote so far

---
+++ MutliRubroProyect/src/test/java/com/example/mutlirubroproyect/MutliRubroProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutlirubroproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutliRubroProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
