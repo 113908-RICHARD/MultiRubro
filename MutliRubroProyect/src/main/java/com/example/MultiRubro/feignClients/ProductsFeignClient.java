eec9bbc59a0fa38495c0fa53d6024f7c8d070786
genaro richard <grichard@encode.tech>
2024-07-19 17:09:48 -0300

    update

--- MutliRubroProyect/src/main/java/com/example/MultiRubro/feignClients/ProductsFeignClient.java
+++ MutliRubroProyect/src/main/java/com/example/MultiRubro/feignClients/ProductsFeignClient.java
@@ -4,14 +4,12 @@ package com.example.MultiRubro.feignClients;
 import com.example.MultiRubro.feignClients.feignModels.GenericResponse;
 import com.example.MultiRubro.feignClients.feignModels.UpdateProductStockRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@FeignClient(name = ("MultiRubroProyectProducts"),url = "http://localhost:8081/products")
+@FeignClient(name = "multi-rubro-project-products",path = "/products")
+
 public interface ProductsFeignClient {
 
     @GetMapping("/stock/{id}")
