c1b1bb34e6b5eaa858860169dbe21ab2ad6b10e7
genaro richard <grichard@encode.tech>
2024-07-26 17:01:49 -0300

    update

--- gateway-service/src/main/java/com/springboot/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/springboot/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,10 @@ package com.springboot.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
