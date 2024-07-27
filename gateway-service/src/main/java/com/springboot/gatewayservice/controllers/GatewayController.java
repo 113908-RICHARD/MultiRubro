d983550cce14dffc7aa7c75273b515669be25aab
Genaro Richard <113908@tecnicatura.frc.utn.edu.ar>
2024-07-27 20:44:58 -0300

    update

---
+++ gateway-service/src/main/java/com/springboot/gatewayservice/controllers/GatewayController.java
@@ -0,0 +1,24 @@
+package com.springboot.gatewayservice.controllers;
+
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class GatewayController {
+
+
+    @GetMapping
+    public Mono<String> index(WebSession session) {
+        return Mono.just(session.getId());
+
+    }
+    @GetMapping("/token")
+    public Mono<String> getToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client){
+        return  Mono.just(client.getAccessToken().getTokenValue());
+    }
+}
---
+++ gateway-service/src/main/java/com/springboot/gatewayservice/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.springboot.gatewayservice.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+
+                        .anyExchange().authenticated())
+                .oauth2Login(withDefaults());
+
+        return http.build();
+    }
+}
