e3e0cfd361556a36ad4ff0be427435afb2da0159
Genaro Richard <113908@tecnicatura.frc.utn.edu.ar>
2024-07-27 23:54:26 -0300

    update

---
+++ MutliRubroProyect/src/main/java/com/example/MultiRubro/configs/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.MultiRubro.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.service.invoker.AbstractReactorHttpExchangeAdapter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 ->oauth2.jwt(withDefaults())).build();
+    }
+
+
+}
