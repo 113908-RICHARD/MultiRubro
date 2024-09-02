5dc47fb19eacd0b2df90fb72cbb3c8ba1ff79d8d
Genaro Richard <113908@tecnicatura.frc.utn.edu.ar>
2024-09-02 11:35:57 -0300

    update

--- MutliRubroProyect/src/main/java/com/example/MultiRubro/configs/RabbitConfig.java
+++ MutliRubroProyect/src/main/java/com/example/MultiRubro/configs/RabbitConfig.java
@@ -3,6 +3,7 @@ package com.example.MultiRubro.configs;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -28,4 +29,14 @@ public class RabbitConfig {
         rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
     }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+    @Bean
+    public Object createQueue(RabbitAdmin rabbitAdmin) {
+       return rabbitAdmin.declareQueue(queue()); // Declare the queue
+    }
+
 }
